0db38b1e54fbf40a981b131ab5eea39a42d652b3
YogeshModi24 <info.yogessh@gmail.com>
2026-06-28 19:52:54 +0530

    feat(db): implement index.d repository

---
+++ packages/repositories/src/index.d.ts
@@ -0,0 +1,7 @@
+export * from './factories/RepositoryFactory';
+export * from './interfaces/IIssueRepository';
+export * from './interfaces/INotificationRepository';
+export * from './interfaces/IUnitOfWork';
+export * from './interfaces/IUserRepository';
+export * from './interfaces/IUserSessionRepository';
+//# sourceMappingURL=index.d.ts.map
