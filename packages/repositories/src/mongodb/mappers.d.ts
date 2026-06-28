af31fb74c944ca883dd63a15a7eee7df59b60210
YogeshModi24 <info.yogessh@gmail.com>
2026-06-28 19:52:55 +0530

    feat(db): implement mappers.d repository

---
+++ packages/repositories/src/mongodb/mappers.d.ts
@@ -0,0 +1,6 @@
+import { Issue, Notification, User, UserSession } from '@community-os/types';
+export declare function mapMongoUser(doc: any): User;
+export declare function mapMongoIssue(doc: any): Issue;
+export declare function mapMongoUserSession(doc: any): UserSession;
+export declare function mapMongoNotification(doc: any): Notification;
+//# sourceMappingURL=mappers.d.ts.map
