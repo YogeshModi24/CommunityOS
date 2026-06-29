02370d3408f4fa1503d6da80cabe194dc1d06831
YogeshModi24 <info.yogessh@gmail.com>
2026-06-29 19:54:51 +0530

    fix(deploy): inject NEXT_PUBLIC_API_URL

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -10,7 +10,8 @@ const nextConfig = {
   env: {
     AUTH_TRUST_HOST: "true",
     NEXTAUTH_URL: "https://community-os-web-seven.vercel.app",
-    AUTH_SECRET: "0123456789abcdef0123456789abcdef0123456789abcdef"
+    AUTH_SECRET: "0123456789abcdef0123456789abcdef0123456789abcdef",
+    NEXT_PUBLIC_API_URL: "https://communityos-0d4d.onrender.com"
   },
 };
 
